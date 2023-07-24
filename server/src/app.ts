c70666fdbeda3b29e2378f1223494de04d0d3341
jarvis2f <>
2023-07-24 09:57:11 +0800

    #14 Increase the size of HTTP requests to 20mb.

--- server/src/app.ts
+++ server/src/app.ts
@@ -37,8 +37,8 @@ export const startServer = async () => {
     app.use(res_handler);
     app.use(cors());
     app.all('/api/u/stripe/webhook', express.raw({type: 'application/json'}));
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
+    app.use(express.json({limit: '20mb'}));
+    app.use(express.urlencoded({limit: '20mb', extended: true}));
     app.use(auth);
     app.use('/v1/*', getLimiter())
     app.use(relay_auth);
